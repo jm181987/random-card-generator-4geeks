1a43b88b696341a485f0c83f09a4cada2cde150f
jm181987 <jorgitom18@gmail.com>
2022-11-12 17:53:56 +0000

    corrigiendo

--- src/app.js
+++ src/app.js
@@ -1,54 +1,52 @@
-window.onload = function (){
-   
-  function randomCard () {
-      let randomSuite = Math.ceil(Math.random()*4);
-      let suite = "";
+window.onload = function() {
+  function randomCard() {
+    let randomSuite = Math.ceil(Math.random() * 4);
+    let suite = "";
 
-      switch(randomSuite){
-          case 1:
-              suite = "&hearts;";
-              break;
-          case 2:
-              suite = "&diams;";
-              break;
-          case 3:
-              suite = "&clubs;";
-              break;
-          case 4:
-              suite = "&spades;";
-              break;
-      }
-      console.log(suite);
-      return suite;
+    switch (randomSuite) {
+      case 1:
+        suite = "&hearts;";
+        break;
+      case 2:
+        suite = "&diams;";
+        break;
+      case 3:
+        suite = "&clubs;";
+        break;
+      case 4:
+        suite = "&spades;";
+        break;
+    }
+    console.log(suite);
+    return suite;
   }
 
   let resultCard = randomCard();
 
-  function randomCardNumber () {
-      let randomNum = Math.ceil(Math.random()*13);
-      let numberCard = "";
+  function randomCardNumber() {
+    let randomNum = Math.ceil(Math.random() * 13);
+    let numberCard = "";
 
-      switch(randomNum){
-        case 1:
-          numberCard = "A";
-          break; 
-        
-        case 11:
-              numberCard = "J";
-              break;
-          case 12:
-              numberCard = "Q";
-              break;
-          case 13:
-              numberCard = "K";
-              break;
-          default:
-              numberCard = randomNum
-              break;
-          
-      }
-      console.log(numberCard);
-      return numberCard;
+    switch (randomNum) {
+      case 1:
+        numberCard = "A";
+        break;
+
+      case 11:
+        numberCard = "J";
+        break;
+      case 12:
+        numberCard = "Q";
+        break;
+      case 13:
+        numberCard = "K";
+        break;
+      default:
+        numberCard = randomNum;
+        break;
+    }
+    console.log(numberCard);
+    return numberCard;
   }
 
   let resultNumber = randomCardNumber();
@@ -57,18 +55,16 @@ window.onload = function (){
   let cardFooter = document.querySelector(".card-footer");
   let cardBody = document.querySelector(".card-body");
 
-  
-  if(resultCard == "&hearts;" || resultCard == "&diams;"){
-      cardHeader.style.color = "red";
-      cardHeader.innerHTML = resultCard;
-      cardFooter.style.color = "red";
-      cardFooter.innerHTML = resultCard;
-      cardBody.style.color = "red";
-      cardBody.innerHTML = resultNumber;
+  if (resultCard == "&hearts;" || resultCard == "&diams;") {
+    cardHeader.style.color = "red";
+    cardHeader.innerHTML = resultCard;
+    cardFooter.style.color = "red";
+    cardFooter.innerHTML = resultCard;
+    cardBody.style.color = "red";
+    cardBody.innerHTML = resultNumber;
   } else {
-      cardHeader.innerHTML = resultCard;
-      cardFooter.innerHTML = resultCard;
-      cardBody.innerHTML = resultNumber;
+    cardHeader.innerHTML = resultCard;
+    cardFooter.innerHTML = resultCard;
+    cardBody.innerHTML = resultNumber;
   }
-
-}
+};
